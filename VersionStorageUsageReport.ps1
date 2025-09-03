c34770e52e3995ffc0be02e557612111de3c974e
Dakota R <druhl@enchantedrock.com>
2025-09-03 14:29:20 -0500

    Updated for ERIntranet site and report location - running currently

--- VersionStorageUsageReport.ps1
+++ VersionStorageUsageReport.ps1
@@ -4,8 +4,12 @@ Import-Module Microsoft.Online.SharePoint.PowerShell -UseWindowsPowerShell
 #Connect SPO Site
 Connect-SPOService -Url https://enchantedrock-admin.sharepoint.com
 
+#Configure Target Site and Report URL
+$siteUrl = "https://enchantedrock.sharepoint.com/sites/erintranet"
+$reportUrl = "https://enchantedrock.sharepoint.com/sites/erintranet/IT%20Corporate/VersionStorageUsageReport.csv"
+
 #Generate a version storage usage report for a site or OneDrive account 	
-New-SPOSiteFileVersionExpirationReportJob -Identity $siteUrl -ReportUrl $reportUrl
+New-SPOSiteFileVersionExpirationReportJob -Identity $siteUrl -ReportUrl $reportUrl -ErrorAction Stop -Debug
 
 #Track progress of the job to generate report for a site or OneDrive account 	
 Get-SPOSiteFileVersionExpirationReportJobProgress -Identity $siteUrl -ReportUrl $reportUrl
