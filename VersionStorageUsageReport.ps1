2c0f4dec9af540de21e43c31b071f6994649ed32
Dakota R <druhl@enchantedrock.com>
2025-09-05 10:42:27 -0500

    Added Library

--- VersionStorageUsageReport.ps1
+++ VersionStorageUsageReport.ps1
@@ -6,16 +6,17 @@ Connect-SPOService -Url https://enchantedrock-admin.sharepoint.com
 
 #Configure Target Site and Report URL
 $siteUrl = "https://enchantedrock.sharepoint.com/sites/erintranet"
-$reportUrl = "https://enchantedrock.sharepoint.com/sites/erintranet/IT%20Corporate/VersionStorageUsageReport.csv"
+$reportUrl = "https://enchantedrock.sharepoint.com/sites/erintranet/IT Corporate/Reports in progress - restricted/VersionStorageUsageReport_OMLibrary.csv"
+$libName = "O&M"
 
 #Generate a version storage usage report for a site or OneDrive account 	
-New-SPOSiteFileVersionExpirationReportJob -Identity $siteUrl -ReportUrl $reportUrl -ErrorAction Stop -Debug
+####New-SPOSiteFileVersionExpirationReportJob -Identity $siteUrl -ReportUrl $reportUrl -ErrorAction Stop -Debug
 
 #Track progress of the job to generate report for a site or OneDrive account 	
-Get-SPOSiteFileVersionExpirationReportJobProgress -Identity $siteUrl -ReportUrl $reportUrl
+##Get-SPOSiteFileVersionExpirationReportJobProgress -Identity $siteUrl -ReportUrl $reportUrl
 
 #Generate a version storage usage report for a library 	
-New-SPOListFileVersionExpirationReportJob -Site $siteUrl -List $libName -ReportUrl $reportUrl
+####New-SPOListFileVersionExpirationReportJob -Site $siteUrl -List $libName -ReportUrl $reportUrl
 
 #Track progress of the job to generate report for a library 	
-Get-SPOListFileVersionExpirationReportJobProgress -Site $siteUrl -List $libName -ReportUrl $reportUrl
+##Get-SPOListFileVersionExpirationReportJobProgress -Site $siteUrl -List $libName -ReportUrl $reportUrl
