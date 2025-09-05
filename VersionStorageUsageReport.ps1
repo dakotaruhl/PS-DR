ba5163eb962a39c2f9516c8ea011b315083b0e4d
Dakota R <druhl@enchantedrock.com>
2025-09-05 10:55:55 -0500

    Added current jobs

--- VersionStorageUsageReport.ps1
+++ VersionStorageUsageReport.ps1
@@ -9,19 +9,29 @@ $siteUrl = "https://enchantedrock.sharepoint.com/sites/erintranet"
 $reportUrl = "https://enchantedrock.sharepoint.com/sites/erintranet/IT Corporate/Reports in progress - restricted/VersionStorageUsageReport_EPCLibrary.csv"
 $libName = "EPC"
 
+<#
 #Generate a version storage usage report for a site or OneDrive account 	
-####New-SPOSiteFileVersionExpirationReportJob -Identity $siteUrl -ReportUrl $reportUrl 
+New-SPOSiteFileVersionExpirationReportJob -Identity $siteUrl -ReportUrl $reportUrl 
+#>
 
-#Track progress of the job to generate report for a site or OneDrive account 	
-##Get-SPOSiteFileVersionExpirationReportJobProgress -Identity $siteUrl -ReportUrl $reportUrl
+<#
+#Track progress of the job to generate report for a site or OneDrive account 
+#Current Jobs
+$reportUrl = "https://enchantedrock.sharepoint.com/sites/erintranet/IT Corporate/Reports in progress - restricted/VersionStorageUsageReport_FullSite.csv"	
+Get-SPOSiteFileVersionExpirationReportJobProgress -Identity $siteUrl -ReportUrl $reportUrl
+#>
 
+<#
 #Generate a version storage usage report for a library 	
-####New-SPOListFileVersionExpirationReportJob -Site $siteUrl -List $libName -ReportUrl $reportUrl
+New-SPOListFileVersionExpirationReportJob -Site $siteUrl -List $libName -ReportUrl $reportUrl
+#>
 
 #Track progress of the job to generate report for a library 
-<#
 #Current Jobs
+<#
 $libName = "EPC"
+$reportUrl = "https://enchantedrock.sharepoint.com/sites/erintranet/IT Corporate/Reports in progress - restricted/VersionStorageUsageReport_EPCLibrary.csv"
 $libName = "O&M"	
+$reportUrl = "https://enchantedrock.sharepoint.com/sites/erintranet/IT Corporate/Reports in progress - restricted/VersionStorageUsageReport_OMLibrary.csv"
 Get-SPOListFileVersionExpirationReportJobProgress -Site $siteUrl -List $libName -ReportUrl $reportUrl
 #>
