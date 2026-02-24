0c248bfd8ee10ff829b3baa4c859a36171c0b964
Dakota R <druhl@enchantedrock.com>
2026-02-24 16:03:30 -0600

    Enhance CDN_SPO.ps1 to add an asset library for Office templates and update thumbnail settings; modify OwlRock.ps1 to manage mailbox folder permissions for room calendars.

--- CDN_SPO.ps1
+++ CDN_SPO.ps1
@@ -13,3 +13,10 @@ Get-SPOTenantCdnOrigins -CdnType Public
 
 #Policy
 Get-SPOTenantCdnPolicies -CdnType Public
+
+https://enchantedrock.sharepoint.com/sites/BrandGuide/Image Assets/Office Templates Thumbnail.jpg
+
+#Brand Center - Add an asset library to the organization assets
+Add-SPOOrgAssetsLibrary -LibraryUrl "https://enchantedrock.sharepoint.com/sites/BrandGuide/Office Template Assets" -OrgAssetType OfficeTemplateLibrary -CdnType Public -ThumbnailUrl "https://enchantedrock.sharepoint.com/sites/BrandGuide/Image Assets/Office Templates Thumbnail.jpg"
+get-SPOOrgAssetsLibrary
+Set-SPOOrgAssetsLibrary -LibraryUrl "https://enchantedrock.sharepoint.com/sites/BrandGuide/Image Assets" -ThumbnailUrl "https://enchantedrock.sharepoint.com/sites/BrandGuide/Image Assets/Logos/ER-Arch-Color.jpg"
--- OwlRock.ps1
+++ OwlRock.ps1
@@ -1,4 +1,16 @@
+Connect-ExchangeOnline
+
 Get-CalendarProcessing -Identity "Owl Rock" | Format-List AllowDistributionGroup,BookInPolicy,RequestInPolicy,RequestOutOfPolicy
 
 Get-DistributionGroup -identity "/o=ExchangeLabs/ou=Exchange Administrative Group (FYDIBOHF23SPDLT)/cn=Recipients/cn=d72ddffa90cb4129afd36639e698c064-OwlRockSG20" | FL
 Get-DistributionGroupMember -identity "/o=ExchangeLabs/ou=Exchange Administrative Group (FYDIBOHF23SPDLT)/cn=Recipients/cn=d72ddffa90cb4129afd36639e698c064-OwlRockSG20"
+
+
+#access to room calendars
+$calendarIdentity = "owlrock@enchantedrock.com:\Calendar"
+$userIdentityAdd = "kparekh@enchantedrock.com"
+$userIdentityRemove = "Nikole Elliott-Harvest"
+
+Add-MailboxFolderPermission -Identity $calendarIdentity -User $userIdentityAdd -AccessRights editor
+Get-MailboxFolderPermission -Identity $calendarIdentity | Format-Table User,AccessRights -AutoSize 
+Remove-MailboxFolderPermission -Identity $calendarIdentity -User $userIdentityRemove
