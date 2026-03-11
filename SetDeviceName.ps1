ddd9cba60048c109cf507c17a3e57e38d3b42126
Dakota R <druhl@enchantedrock.com>
2026-03-11 14:49:46 -0500

    Refactor SetDeviceName.ps1 to improve device renaming logic; streamline user name handling and enhance error reporting; export detailed results to Excel files. First commit before testing

--- SetDeviceName.ps1
+++ SetDeviceName.ps1
@@ -1,20 +1,17 @@
-Import-Module -Name Microsoft.Graph.DeviceManagement
 Import-Module -Name ImportExcel
+Connect-MgGraph -Scopes "DeviceManagementManagedDevices.PrivilegedOperations.All"
+$ErrorActionPreference = 'Stop'
 
-Connect-MgGraph -Scopes "DeviceManagementManagedDevices.ReadWrite.All"
-
+#Get all devices
 $devices = Get-MgDeviceManagementManagedDevice
-# Filter for the specific device, for example, by its current name or serial number
-#$device = $devices | Where-Object {$_.DeviceName -eq "MalloryBlackwell"}
+#$device = $devices | Where-Object {$_.DeviceName -eq "DESKTOP-OIO36FL"}
+#$device | FL
 
+#Import device user names 
+$DisplayNamesExcel = Import-Excel -Path "C:\Users\DakotaRuhl\Documents\Reports\Devices\ManufDeviceReport.xlsx"  -WorksheetName "Test Display Names"
+$DisplayNamesExcel.count 
 
-#manual import device user names 
-$DisplayNamesExcel = Import-Excel -Path "C:\Users\DakotaRuhl\Documents\Reports\Devices\ManufDeviceReport.xlsx"  -WorksheetName "User Display Names"
-$device = $devices | Where-Object {$_.UserDisplayName -eq "Tyler Lauw"}
-
-##Invoke Option
-#Doing last 7 of serial number - First 3 of First Name, First 4 of Last Name
-
+#Create tracking variables for summary and details of results
 $summary = @{
     SuccessfullyRenamed = 0
     AlreadyCorrectName = 0
@@ -22,71 +19,119 @@ $summary = @{
     Errors = 0
 }
 
-Connect-MgGraph -Scopes "DeviceManagementManagedDevices.PrivilegedOperations.All" 
+$SuccessfullyRenamedList = @()
+$AlreadyCorrectNameList = @()
+$NotFoundList = @()
+$ErrorsList = @()
 
-foreach ($user in $DisplayNamesExcel) {
-    $device = $devices | Where-Object {$_.UserDisplayName -eq $user.DisplayName}
-    if ($device) 
+##Invoke API to rename devices - Format: SerialNumber-FirstInitialLastName (up to 6 characters of last name)
+Function Rename-DeviceForUser ([Microsoft.Graph.PowerShell.Models.IMicrosoftGraphManagedDevice]$device, $newDeviceName)
+{
+    if ($device.DeviceName -ne $newDeviceName) 
     {
-        $firstName = $device.UserDisplayName.Split(' ')[0]
-        $lastName = $device.UserDisplayName.Split(' ')[1]
-        $FirstNameLetters = $firstName.Substring(0,1).ToUpper() + ($firstName.Substring(1,2)).ToLower()
-        $LastNameLetters = ($lastName.Substring(0,1)).ToUpper() + ($lastName.Substring(1,3)).ToLower()
-        $newDeviceName = "$($device.SerialNumber.Substring($device.SerialNumber.Length - 7))-$FirstNameLetters$LastNameLetters"
-        
-        if ($device.DeviceName -ne $newDeviceName) 
+        Write-Host "Renaming device '$($device.DeviceName)'($($device.SerialNumber)) to $newDeviceName" -ForegroundColor Green
+        try 
         {
-            Write-Host "Renaming device with serial $($user.SerialNumber) from $($device.DeviceName) to $newDeviceName" -ForegroundColor Green
-            
-            try 
-            {
-                $serialNumber = $device.Serialnumber
-                $apiUrl = "https://graph.microsoft.com/beta/deviceManagement/managedDevices?`$filter=serialNumber eq '$serialNumber'"
-                $deviceResult = Invoke-MgGraphRequest -Method GET -Uri $apiUrl
+            $serialNumber = $device.Serialnumber
+            $apiUrl = "https://graph.microsoft.com/beta/deviceManagement/managedDevices?`$filter=serialNumber eq '$serialNumber'"
+            $deviceResult = Invoke-MgGraphRequest -Method GET -Uri $apiUrl
+    
+            $intuneDeviceId = $deviceResult.value[0].id
+            $currentDeviceName = $deviceResult.value[0].deviceName
         
-                $intuneDeviceId = $deviceResult.value[0].id
-                $currentDeviceName = $deviceResult.value[0].deviceName
-            
-                $body = @{ deviceName = $newDeviceName } | ConvertTo-Json
-                $updateUrl = "https://graph.microsoft.com/beta/deviceManagement/managedDevices/$intuneDeviceId/setDeviceName"
+            $body = @{ deviceName = $newDeviceName } | ConvertTo-Json
+            $updateUrl = "https://graph.microsoft.com/beta/deviceManagement/managedDevices/$intuneDeviceId/setDeviceName"
 
-                Invoke-MgGraphRequest -Method POST -Uri $updateUrl -Body $body
-                Write-Host "Successfully renamed device with serial $serialNumber from $currentDeviceName to $newDeviceName" -ForegroundColor Green
-                $summary.SuccessfullyRenamed++
+            Invoke-MgGraphRequest -Method POST -Uri $updateUrl -Body $body
+            Write-Host "Successfully renamed device with serial $serialNumber from $currentDeviceName to $newDeviceName" -ForegroundColor Green
+
+            $summary.SuccessfullyRenamed++
+            $SuccessfullyRenamedList += [PSCustomObject]@{
+                DeviceUser = $device.UserDisplayName
+                SerialNumber = $serialNumber
+                OldDeviceName = $currentDeviceName
+                NewDeviceName = $newDeviceName
             }
-            catch 
-            {
-                Write-Host "Error processing device with serial $serialNumber : $_" -ForegroundColor Red
-                $summary.Errors++
-            }
-        } 
-        else 
+        }
+        catch 
         {
-            Write-Host "Device with serial $serialNumber already has the correct name: $currentDeviceName" -ForegroundColor Cyan
-            $summary.AlreadyCorrectName++
-            continue
+            Write-Host "Error processing device with serial $($device.Serialnumber) : $_.Exception.Message" -ForegroundColor Red
+            $summary.Errors++
+            $ErrorsList += [PSCustomObject]@{
+                DeviceUser = $device.UserDisplayName
+                SerialNumber = $serialNumber
+                ErrorMessage = $_.Exception.Message
+            }
         }
     } 
     else 
     {
-        Write-Host "No device found with serial number: $serialNumber" -ForegroundColor Yellow
-        $summary.NotFound++
+        Write-Host "Device with serial $($device.Serialnumber) already has the correct name: $currentDeviceName" -ForegroundColor Cyan
+        $summary.AlreadyCorrectName++
+        $AlreadyCorrectNameList += [PSCustomObject]@{
+            DeviceUser = $device.UserDisplayName
+            SerialNumber = $device.Serialnumber
+            DeviceName = $currentDeviceName
+        }
         continue
     }
+
 }
 
+#Main loop to process each user and their devices
+foreach ($user in $DisplayNamesExcel) 
+{
+    $device = $devices | Where-Object {$_.UserDisplayName -eq $user.DisplayName}
 
-#output serial numbers with length of serial numbers 
-$SerialNumbers = @()
-foreach ($device in $devices) {
-    $SerialNumbers += [PSCustomObject]@{
-        deviceModel = $device.Model
-        deviceUser = $device.UserDisplayName
-        deviceName = $device.DeviceName
-        SerialNumber = $device.Serialnumber
-        Length = $device.Serialnumber.Length
-        Manufacturer = $device.Manufacturer
+    foreach ($d in $device) 
+    {
+        If (($null -ne $d.SerialNumber) -and ($d)) 
+        {
+            Write-Host "Processing $((@($device)).Count) devices for user: $($user.DisplayName)" -ForegroundColor Blue
+
+            #Determine first and last names
+            $firstName = $user.DisplayName.Split(' ')[0]
+            $lastName = $user.DisplayName.Split(' ')[1]
+
+            ##testing
+            #$firstName = "Jason"
+            #$lastName = "O’Roark"
+
+            #replace anything that is not a number, letter, or - with nothing
+            $firstName = $firstName -replace '[^a-zA-Z0-9-]', ''
+            $lastName = $lastName -replace '[^a-zA-Z0-9-]', ''
+            
+            #Format first initial of First Name
+            $fnlnCapLetters = $firstName.Substring(0,1).ToUpper() + $lastName.Substring(0,1).ToUpper()
+            #Format up to 6 characters of last name (first letter uppercase, rest lowercase)
+            switch ($lastName.Length) 
+            {
+                2 {$lnRemLetters = $lastName.Substring(1,1).ToLower()} 
+                3 {$lnRemLetters = $lastName.Substring(1,2).ToLower()    }
+                4 {$lnRemLetters = $lastName.Substring(1,3).ToLower()    }
+                5 {$lnRemLetters = $lastName.Substring(1,4).ToLower()    }
+                Default {$lnRemLetters = $lastName.Substring(1,5).ToLower()}
+            }
+
+            $newDeviceName = "$($device.SerialNumber.Substring($device.SerialNumber.Length - 7))-$fnlnCapLetters$lnRemLetters"
+            #call function to rename device
+            Rename-DeviceForUser -device $device
+        }
+        else 
+        {
+            Write-Host "Device not found: $($d)" -ForegroundColor Yellow
+            $summary.NotFound++
+            $NotFoundList += [PSCustomObject]@{
+                User = $user.DisplayName
+            }
+            continue
+        }
     }
 }
 
-$SerialNumbers | Export-Excel "C:\Users\DakotaRuhl\Documents\Reports\Devices\ManufDeviceReport.xlsx" -WorksheetName "SerialNumbers" -AutoSize -TableName "SerialNumbersData" -ClearSheet
+Write-Host "Summary: $($summary | ConvertTo-Json -Depth 5)" -ForegroundColor Magenta
+$SuccessfullyRenamedList | Export-Excel -Path "C:\Users\DakotaRuhl\Documents\Reports\Devices\SuccessfullyRenamedDevices.xlsx" -WorksheetName "Renamed Devices" -AutoSize
+$AlreadyCorrectNameList | Export-Excel -Path "C:\Users\DakotaRuhl\Documents\Reports\Devices\AlreadyCorrectNameDevices.xlsx" -WorksheetName "Already Correct Name Devices" -AutoSize
+$NotFoundList | Export-Excel -Path "C:\Users\DakotaRuhl\Documents\Reports\Devices\NotFoundDevices.xlsx" -WorksheetName "Not Found Devices" -AutoSize
+$ErrorsList | Export-Excel -Path "C:\Users\DakotaRuhl\Documents\Reports\Devices\ErrorsRenamingDevices.xlsx" -WorksheetName "Errors Renaming Devices" -AutoSize
+Write-Host "Detailed results exported to Excel files in C:\Users\DakotaRuhl\Documents\Reports\Devices\" -ForegroundColor Magenta
