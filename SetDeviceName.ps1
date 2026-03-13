b8f5931edcceed33c2383eec597cef24d5349355
Dakota R <druhl@enchantedrock.com>
2026-03-13 16:13:18 -0500

    Enhance SetDeviceName.ps1 to improve device user tracking; add functionality for handling multiple users per device, detailed logging of last signed-in users, and export results to a new Excel report.

--- SetDeviceName.ps1
+++ SetDeviceName.ps1
@@ -5,7 +5,8 @@ and renames the devices in Intune to match the expected format.
 It also generates a comprehensive report of the results.
 
 Currently the option to actually rename the device is commented out on line 206 to allow for testing and validation of the script without making changes to Intune.
- #>
+#>
+
 Start-Transcript -Path "C:\Users\DakotaRuhl\Documents\Reports\Devices\Transcripts\SetDeviceName_$(Get-Date -Format 'yyyyMMdd-HHmmss').txt" -NoClobber
 Import-Module -Name ImportExcel
 Connect-MgGraph -Scopes "DeviceManagementManagedDevices.PrivilegedOperations.All" -NoWelcome
@@ -15,10 +16,62 @@ $ErrorActionPreference = 'Stop'
 Write-Host "Retrieving devices from Intune..." -ForegroundColor Blue
 $devices = Get-MgDeviceManagementManagedDevice -All
 
+<# $Workbook = Import-Excel -Path "C:\Users\DakotaRuhl\Documents\Reports\Devices\ManufDeviceReport.xlsx"  -WorksheetName "Sheet1" | 
+    Select-Object DeviceUser, SerialNumber
+$user = $Workbook[8]
+$brokenDevice = $devices | Where-Object {$_.SerialNumber.Split(" ")[0] -eq "1H852048V4"}
+$user = $null
+$brokenDevice = $null
+
+$devicetestlist = @()
+Foreach ($user in $Workbook) 
+{
+    $brokenDevice = $devices | Where-Object {$_.SerialNumber.Split(" ")[0] -eq $user.SerialNumber}
+    $serialNumber = $brokenDevice.SerialNumber.Split(" ")[0]
+    if ($brokenDevice) 
+    {
+        
+        $apiUrl = "https://graph.microsoft.com/beta/deviceManagement/managedDevices?`$filter=serialNumber eq '$serialNumber'"  
+        $deviceResult = Invoke-MgGraphRequest -Method GET -Uri $apiUrl -ErrorAction Stop 
+
+        $lastuserIDs = $deviceResult.value.usersLoggedOn.userId -join (', ')
+        $lastuserTimes = $deviceResult.value.usersLoggedOn.lastLogOnDateTime -join (', ')
+        
+        If((($deviceResult.value.usersLoggedOn.userId).count -gt 1) -or ($brokenDevice.DeviceName.Count -gt 1)) {
+            Write-Host "Multiple users found for serial $($serialNumber). Last signed-in users: $($lastuserIDs)" -ForegroundColor Yellow
+            $devicetestlist += [PSCustomObject]@{
+                User = $user.DeviceUser
+                SerialNumber = $serialNumber
+                DeviceName1 = $brokenDevice.DeviceName[0]
+                DeviceName2 = $brokenDevice.DeviceName[1]
+                OperatingSystem = $brokenDevice.OperatingSystem[0]
+                LastSignedInUsers = $lastuserIDs
+                LastSignedInTimes = $lastuserTimes
+            }
+        }
+        else {
+            Write-Host "Single user found for serial $($serialNumber). Last signed-in user: $($lastuserIDs)" -ForegroundColor Yellow
+            $devicetestlist += [PSCustomObject]@{
+                User = $user.DeviceUser
+                SerialNumber = $serialNumber
+                DeviceName1 = $brokenDevice.DeviceName
+                OperatingSystem = $brokenDevice.OperatingSystem
+                LastSignedInUsers = $lastuserIDs
+                LastSignedInTimes = $lastuserTimes
+            }
+        }
+    }
+    else {
+        continue
+    }
+}
+$devicetestlist | Export-Excel -Path "C:\Users\DakotaRuhl\Documents\Reports\Devices\DeviceTestList.xlsx" -AutoSize -WorksheetName "DeviceTestList"   #>
+
 #Import device user names and ID's from Excel report. Remove empty rows, only select specific column in specific sheet
 $Workbook = Import-Excel -Path "C:\Users\DakotaRuhl\Documents\Reports\Devices\ManufDeviceReport.xlsx"  -WorksheetName "User Display Names with ID" | 
     Select-Object UniqueDisplayName, UniqueUserID | 
     Where-Object { $_.UniqueDisplayName -ne $null -and $_.UniqueDisplayName -ne "" }
+
 
 #Create tracking variables for summary and details of results
 $summary = @{
@@ -353,4 +406,41 @@ Export-ReportWorkbookWithTimestamp -Path $reportPath -Sheets @{
 Write-Host "Comprehensive report with summary exported to: $reportPath" -ForegroundColor Magenta
 Stop-Transcript
 
-$Devices | Export-Excel -Path "$basePath\AllDevices.xlsx" -WorksheetName "All Devices" -AutoSize -Append
+################
+
+<#Import-Module Microsoft.Graph.Users
+Connect-Graph -scopes "User.Read.All"
+
+ $userIDs = Import-Excel -Path "C:\Users\DakotaRuhl\Documents\Reports\Devices\DeviceTestList.xlsx"  -WorksheetName "DeviceTestList" | 
+    Select-Object SerialNumber, LastSignedInUsers
+
+##$obj = $userids[0]
+##$obj = $null
+
+$displayNames = @()
+foreach ($obj in $userIDs)
+{
+    
+    $i = 0
+    $ids = $obj.LastSignedInUsers.Split(',').Trim()
+    if(-not($obj.LastSignedInUsers))
+    {
+        continue
+    }
+    else 
+    {
+        while ($i -lt $ids.Count) 
+        {
+            $name = Get-MgUser -userid $obj.LastSignedInUsers.Split(',').Trim()[$i] | Select-Object DisplayName
+            $displayNames += [PSCustomObject]@{ 
+                SerialNumber = $obj.SerialNumber.Split(" ")[0]
+                UserID = $obj.LastSignedInUsers.Split(',').Trim()[$i]
+                DisplayName = $name.DisplayName 
+            }
+            $i++
+        }
+    }
+    
+}
+
+$displayNames | Export-Excel -Path "C:\Users\DakotaRuhl\Documents\Reports\Devices\DeviceTestListNames.xlsx" -AutoSize -WorksheetName "DeviceTestListNames"    #>
