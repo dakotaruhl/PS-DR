8adf1729a0646db3fb50a936d42a687d92b4d584
Dakota R <druhl@enchantedrock.com>
2026-03-18 18:26:35 -0500

    Add RoomsCalendarPerms.ps1 to manage calendar permissions for room mailboxes; implement user addition and removal with logging of changes.

---
+++ RoomsCalendarPerms.ps1
@@ -0,0 +1,60 @@
+#Connect-ExchangeOnline
+
+#User to add/Remove
+$userIdentityAdd = "Room Calendar Editors"
+$userIdentityRemove = "Tiffany Amaya"
+
+# Get all rooms
+$teamsRooms = Get-Mailbox -RecipientTypeDetails RoomMailbox
+#$room = $teamsRooms[0]
+$calendarPermissions = @()
+# Iterate through each room and update permissions 
+foreach ($room in $teamsRooms) 
+{
+    $calendarIdentity = "$($room.primarySmtpAddress):\Calendar"
+    $newPermissions = Get-MailboxFolderPermission -Identity $calendarIdentity
+
+    if( ($newPermissions.user | ForEach-Object { $_.ToString() }) -notcontains $userIdentityAdd) 
+    {
+        # Add the user with Editor permissions if they don't already have permissions
+        Add-MailboxFolderPermission -Identity $calendarIdentity -User $userIdentityAdd -AccessRights Editor
+        $newPermissions = Get-MailboxFolderPermission -Identity $calendarIdentity
+        Write-Host "Added $userIdentityAdd to $($room.DisplayName) permissions." -ForegroundColor Green
+    }
+    else {
+         Write-Host "$userIdentityAdd already has permissions for $($room.DisplayName). Skipping..." -ForegroundColor Yellow
+    }
+
+    if( ($newPermissions.user | ForEach-Object { $_.ToString() }) -contains $userIdentityRemove) 
+    {
+        # Remove the user if they have permissions
+        Remove-MailboxFolderPermission -Identity $calendarIdentity -User $userIdentityRemove -Confirm:$false
+        $newPermissions = Get-MailboxFolderPermission -Identity $calendarIdentity
+        Write-Host "Removed $userIdentityRemove from $($room.DisplayName) permissions." -ForegroundColor Red
+    }
+    else {
+         Write-Host "$userIdentityRemove does not have permissions for $($room.DisplayName). Skipping..." -ForegroundColor Yellow
+    }
+    
+    Write-Host "Updated permissions for $($room.DisplayName):" -ForegroundColor Green
+    $newPermissions | Format-Table User,AccessRights -AutoSize 
+    $calendarPermissions += $newPermissions |
+        Where-Object { $_.AccessRights -and ($_.AccessRights -notcontains 'None') } |
+        ForEach-Object {
+            [PSCustomObject]@{
+                Room         = $room.DisplayName
+                User         = $_.User.ToString()
+                AccessRights = ($_.AccessRights -join ', ')
+            }
+        }
+}
+
+Export-Excel -Path "C:\Users\DakotaRuhl\Documents\Reports\Calendar Permissions\AllRooms3.xlsx" -WorksheetName "Permissions" -AutoSize -TableName "CalendarPermissions" -InputObject $calendarPermissions
+
+#Remove-MailboxFolderPermission -Identity $calendarIdentity -User $userIdentityRemove
+#Get-MailboxFolderPermission -Identity $calendarIdentity | Format-Table User,AccessRights -AutoSize 
+
+#expand the "Er Calendar Owners" user
+#Get-MailboxFolderPermission -Identity $calendarIdentity -User "ER Calender Owners"
+
+#$allRooms = $teamsRooms | ForEach-Object { $_.DisplayName }
--- SPOUserIDMismatch.ps1
+++ SPOUserIDMismatch.ps1
@@ -1,7 +1,7 @@
 #Set Parameters
 $AdminCenterURL="https://enchantedrock-admin.sharepoint.com/"
 #user with the ID mismatch
-$UserLoginID = "i:0#.f|membership|manguiano@yourcompany.com"
+$UserLoginID = "i:0#.f|membership|manguiano@enchantedrock.com"
 #enter user, that is sharepoint admin
 $SiteCollectionAdmin = "admin-dr@enchantedrock.com"
 
@@ -10,10 +10,10 @@ Import-Module Microsoft.Online.SharePoint.PowerShell -UseWindowsPowerShell
 
 #Connect to SharePoint Online
 Connect-SPOService -Url $AdminCenterURL
- 
 
 #Get all Personal Site collections
 $PSitesUrl = Get-SPOSite -Template "SPSPERS" -limit ALL -includepersonalsite $True | Select URL
+
 
 Foreach ($PSiteUrl in $PSitesUrl.url)
 {
@@ -21,6 +21,7 @@ Foreach ($PSiteUrl in $PSitesUrl.url)
 	Set-SPOUser -site $PSiteUrl -LoginName $SiteCollectionAdmin -IsSiteCollectionAdmin $True
     
 	#remove the user from "All people" personal site
+	$PSiteUrl = "https://enchantedrock-my.sharepoint.com/personal/ealzner_enchantedrock_com"
 	Remove-SPOUser -Site $PSiteUrl -LoginName $UserLoginID
 
 	#Remove Site collection Admin
@@ -33,3 +34,20 @@ $SiteCollectionAdmin = "admin-dr@enchantedrock.com"
 
 Get-SPOUser -Site $PSiteUrl -LoginName $SiteCollectionAdmin | FL
 Set-SPOUser -site $PSiteUrl -LoginName $SiteCollectionAdmin -IsSiteCollectionAdmin $False
+
+### Single Site Removal ###
+
+##Check UPN on site access list
+Get-SPOUser https://enchantedrock-my.sharepoint.com/personal/ealzner_enchantedrock_com | Where-Object -Property LoginName -eq 'manguiano@enchantedrock.com'
+
+##Set OneDrive site, admin, and User to remove
+$PSiteUrl = "https://enchantedrock-my.sharepoint.com/personal/ealzner_enchantedrock_com"
+$SiteCollectionAdmin = "admin-dr@enchantedrock.com"
+$UserLoginID = "i:0#.f|membership|manguiano@enchantedrock.com"
+
+#Remove user from access list
+Remove-SPOUser -Site $PSiteUrl -LoginName $UserLoginID
+
+#Remove yourself from SiteCollectionAdmins
+Set-SPOUser -site $PSiteUrl -LoginName $SiteCollectionAdmin -IsSiteCollectionAdmin $False
+Get-SPOUser -Site $PSiteUrl -LoginName $SiteCollectionAdmin | FL
